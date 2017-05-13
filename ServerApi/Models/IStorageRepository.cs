5929bbbfbf7f48031989bcb4908175c240ade4aa
Vitaliy.Tartunov <Vitaliy.Tartunov@GMail.com>
2017-05-13 13:17:35 +0700

    First version of server api with .NET Core

---
+++ ServerApi/Models/IStorageRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ServerApi.Models
+{
+    public interface IStorageRepository<StoredValue>
+    {
+        void Add(StoredValue item);
+        IEnumerable<StoredValue> GetAll();
+        StoredValue Find(long key);
+        void Remove(long key);
+        void Update(StoredValue item);
+    }
+}
---
+++ ServerApi/Models/StorageContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerApi.Models
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext(DbContextOptions<StorageContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Temperature> Temperatures { get; set; }
+        public DbSet<Humidity> Humidities { get; set; }
+    }
+}
