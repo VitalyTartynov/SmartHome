0e9d4d4cd668a3323a84bfdf93be735594099242
Vitaly Tartynov <Vitaly.Tartynov@GMail.com>
2018-11-17 22:14:49 +0700

    Refactor names

--- embedded/BasicSensor/BasicSensor.ino
+++ embedded/BasicSensor/BasicSensor.ino
@@ -22,59 +22,58 @@ char THINGSBOARD_HOST[] = "192.168.1.101";
 // ThingsBoard device settings
 #define THINGSBOARD_DEVICE_ID "Hall sensor"
 #define THINGSBOARD_DEVICE_TOKEN "UvYkk1TcD3xZcWOMPw0p"
+#define THINGSBOARD_DEVICE_TELEMETRY_REFRESH 5000 // milliseconds
 
 WiFiClient wifiClient;
 
 // Initialize DHT sensor.
-DHT dht(DHT_PIN, DHT_TYPE);
+DHT dhtSensor(DHT_PIN, DHT_TYPE);
 
 // MQTT client.
-PubSubClient client(wifiClient);
+PubSubClient mqttClient(wifiClient);
 
 int status = WL_IDLE_STATUS;
-unsigned long lastSend;
+unsigned long lastTelemetrySend;
 
 void setup()
 {
   Serial.begin(115200);
-  dht.begin();
+  dhtSensor.begin();
   delay(10);
   InitWiFi();
-  client.setServer(THINGSBOARD_HOST, THINGSBOARD_PORT);
-  lastSend = 0;
+  mqttClient.setServer(THINGSBOARD_HOST, THINGSBOARD_PORT);
+  lastTelemetrySend = 0;
 }
 
 void loop()
 {
-  if (!client.connected()) {
+  if (!mqttClient.connected()) {
     reconnect();
   }
 
-  // Update and send only after 5 seconds
-  if (millis() - lastSend > 5000) {
+  // Update and send telemetry.
+  if (millis() - lastTelemetrySend > THINGSBOARD_DEVICE_TELEMETRY_REFRESH) {
     getAndSendTemperatureAndHumidityData();
-    lastSend = millis();
+    lastTelemetrySend = millis();
   }
 
-  client.loop();
+  mqttClient.loop();
 }
 
 void getAndSendTemperatureAndHumidityData()
 {
-  Serial.println("Collecting sensors telemetry.");
-
   // Reading temperature or humidity takes about 250 milliseconds!
-  float humidity = dht.readHumidity();
+  float humidity = dhtSensor.readHumidity();
   // Read temperature as Celsius (the default)
-  float temperature = dht.readTemperature();
+  float temperature = dhtSensor.readTemperature();
 
   // Check if any reads failed and exit early (to try again).
   if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("Failed to read telemetry from DHT sensor!");
     return;
   }
 
-  float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
+  float heatIndex = dhtSensor.computeHeatIndex(temperature, humidity, false);
 
   Serial.print("Temperature: ");
   Serial.print(temperature);
@@ -98,7 +97,7 @@ void getAndSendTemperatureAndHumidityData()
   // Send payload
   char attributes[100];
   payload.toCharArray(attributes, 100);
-  client.publish("v1/devices/me/telemetry", attributes);
+  mqttClient.publish("v1/devices/me/telemetry", attributes);
   Serial.println(attributes);
 }
 
@@ -118,9 +117,9 @@ void InitWiFi()
 
 void reconnect() {
   // Loop until we're reconnected
-  while (!client.connected()) {
+  while (!mqttClient.connected()) {
     status = WiFi.status();
-    if ( status != WL_CONNECTED) {
+    if (status != WL_CONNECTED) {
       WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
       while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -130,14 +129,14 @@ void reconnect() {
     }
     Serial.print("Connecting to ThingsBoard node ...");
     // Attempt to connect (clientId, username, password)
-    if ( client.connect(THINGSBOARD_DEVICE_ID, THINGSBOARD_DEVICE_TOKEN, NULL) ) {
-      Serial.println( "[DONE]" );
+    if (mqttClient.connect(THINGSBOARD_DEVICE_ID, THINGSBOARD_DEVICE_TOKEN, NULL)) {
+      Serial.println("[DONE]");
     } else {
-      Serial.print( "[FAILED] [ rc = " );
-      Serial.print( client.state() );
-      Serial.println( " : retrying in 5 seconds]" );
+      Serial.print("[FAILED] [ rc = ");
+      Serial.print(mqttClient.state());
+      Serial.println(" : retrying in 5 seconds]");
       // Wait 5 seconds before retrying
-      delay( 5000 );
+      delay(5000);
     }
   }
 }
